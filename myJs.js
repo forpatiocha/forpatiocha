303e82880cea49d40fc912d0dae966a6ac5f64fc
forpatiocha <61425795+forpatiocha@users.noreply.github.com>
2020-02-25 10:40:18 +0700

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'He luu cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        title: 'Hello cđ!!!',
+        text: 'Thì thực ra là t có điều này muốn hỏi m lâu rồi ấy.',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu đẹp trai vl :<<<<<<< ";
+    var text = " Tại vì cậu đẹp trai lắm ấy :<<<<<";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Nói cho tớ lí do cậu thích tớ đi chứ nhỉ',
         html: true,
         width: 900,
         padding: '3em',
